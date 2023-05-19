4b14c6c0eb68646977c61421e9bddb1cb4013cbb
Frida_Argomedo <130621323+Fridaargomedo@users.noreply.github.com>
2023-05-18 18:03:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from dash import Dash, dcc, html 
 import dash_bootstrap_components as dbc 
-
 import pandas as pd 
+import plotly.express as px 
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+df = pd.read_csv("exams.csv)
+
+app = Dash(
+    external_stylesheets=[dbc.themes.LUX]
+)
 
 app.layout = dbc.Container([
     dbc.Card([
       dbc.CardBody([
-         html.H1("Hello from Dash! In a new environment"),
+         html.H1("Ejemplo de radio button"),
          html.Hr(),
-         html.P("I created and evnironment for this fashboard")
+         html.P("Un ejemplo de uso de radio button")
+         dash_table.DataTable(df.to_dict("records"), page_size=10),
+         dbc.Form([
+             dbc.Row([
+                 dbc.Label("Exam", html_for="radio exams", width=1),
+                 dbc.RadioItems(
+                     id="radio exams",
+                     options = [
          
     ])
   ],
